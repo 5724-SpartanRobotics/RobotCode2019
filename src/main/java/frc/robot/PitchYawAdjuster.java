41543f98dc339a61688d4872b070ef8c9f2f617e
FishDog5000 <FishDog5000@users.noreply.github.com>
2019-02-20 17:12:58 -0500

    Update YawPitchAdjuster.java with new data
    Adjust to fit new calibration data (collected shortly before bag and tag yesterday). Obviously this has not been tested yet.

--- src/main/java/frc/robot/PitchYawAdjuster.java
+++ src/main/java/frc/robot/PitchYawAdjuster.java
@@ -2,6 +2,6 @@ package frc.robot;
 
 public class PitchYawAdjuster {
     public static double GetYawFromPitch(double pitch) {
-        return -1.02254 * pitch - 5.42563;
+        return -1.01157 * pitch - 5.3254;
     }
 }
